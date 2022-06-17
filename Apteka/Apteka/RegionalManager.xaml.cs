795b1d34a1b055f9d3b5ab920ded8546db8b7b6f
ejsiik <72543889+ejsiik@users.noreply.github.com>
2022-06-17 10:21:14 +0200

    Dodano widoki dla pracownika i kierowników
    Wersja mocno poglądowa

---
+++ Apteka/Apteka/RegionalManager.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Apteka
+{
+    /// <summary>
+    /// Logika interakcji dla klasy RegionalManager.xaml
+    /// </summary>
+    public partial class RegionalManager : Window
+    {
+        public RegionalManager()
+        {
+            InitializeComponent();
+        }
+
+        private void ListRegionalManager_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
