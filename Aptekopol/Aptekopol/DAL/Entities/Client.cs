10a9f1e274478f48cf170e858ffa3293b1969234
ejsiik <72543889+ejsiik@users.noreply.github.com>
2022-06-18 18:10:07 +0200

    Klient

---
+++ Aptekopol/Aptekopol/DAL/Entities/Client.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Aptekopol.DAL.Entities
+{
+    using DAL.Repo;
+    internal class Client
+    {
+        #region Properties
+        public sbyte? ID { get; set; }
+
+        public string Login { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Surname { get; set; }
+
+        public bool IsCompany { get; set; }
+
+        public string Name { get; set; }
+
+        public string NIP { get; set; }
+
+        public string City { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string PasswordLastChange { get; set; } //DATE?
+        #endregion
+
+        #region Constructors
+        // Stowrzenie obiektu na podstawie otrzymanych danych z BD
+        public Client(MySqlDataReader reader)
+        {
+            ID = sbyte.Parse(reader["ID"].ToString());
+            Login = reader["Login"].ToString();
+            Firstname = reader["Firstname"].ToString();
+            Surname = reader["Surname"].ToString();
+            IsCompany = bool.Parse(reader["Is_company"].ToString());
+            Name = reader["Name"].ToString();
+            NIP = reader["NIP"].ToString(); 
+            City = reader["City"].ToString();
+            Address = reader["Address"].ToString();
+            Phone = reader["Phone"].ToString();
+            Email = reader["Email"].ToString();
+            Password = reader["Password"].ToString();
+            PasswordLastChange = reader["Password_last_change"].ToString();
+        }
+
+        // Stworzenie obiektu na podstawie podanych danych z ID NULL
+        public Client(string login, string firstname, string surname, bool iscompany, string name, string nip, 
+            string city, string address, string phone, string email, string password, string passwordlastchange)
+        {
+            ID = null;
+            Login = login.Trim();
+            Firstname = firstname.Trim();
+            Surname = surname.Trim();
+            IsCompany = iscompany; //check czy na boolach działa bez niczego
+            Name = name.Trim();
+            NIP = nip.Trim();
+            City = city.Trim();
+            Address = address.Trim();
+            Phone = phone.Trim();
+            Email = email.Trim();
+            Password = password.Trim();
+            PasswordLastChange = passwordlastchange.Trim();
+        }
+
+        public Client(Client client)
+        {
+            ID = client.ID;
+            Login = client.Login;
+            Firstname = client.Firstname;
+            Surname = client.Surname;
+            IsCompany = client.IsCompany;
+            Name = client.Name;
+            NIP = client.NIP;
+            City = client.City;
+            Address = client.Address;
+            Phone = client.Phone;
+            Email = client.Email;
+            Password = client.Password;
+            PasswordLastChange = client.PasswordLastChange;
+        }
+        #endregion
+
+        #region Methods
+        // Metoda generująca string dla INSERT TO
+        public string ToInsert()
+        {
+            return $"('{Login}', '{Firstname}', '{Surname}', '{IsCompany}', '{Name}', '{NIP}',  {City}'," +
+                $" '{Address}', '{Phone}', '{Email}', '{Password}', '{PasswordLastChange}')";
+        }
+
+        // Przeciążenie metody Equals
+        public override bool Equals(object obj)
+        {
+            var client = obj as Client;
+            if (client is null) return false;
+            if (Login.ToLower() != client.Login.ToLower()) return false;
+            if (Firstname.ToLower() != client.Firstname.ToLower()) return false;
+            if (Surname.ToLower() != client.Surname.ToLower()) return false;
+            if (IsCompany != client.IsCompany) return false;
+            if (Name.ToLower() != client.Name.ToLower()) return false;
+            if (NIP.ToLower() != client.NIP.ToLower()) return false;
+            if (City.ToLower() != client.City.ToLower()) return false;
+            if (Address.ToLower() != client.Address.ToLower()) return false;
+            if (Phone.ToLower() != client.Phone.ToLower()) return false;
+            if (Email.ToLower() != client.Email.ToLower()) return false;
+            if (Password.ToLower() != client.Password.ToLower()) return false;
+            if (PasswordLastChange.ToLower() != client.PasswordLastChange.ToLower()) return false;
+
+            return true;
+        }
+
+        // Funkcja do zwracania hashu obiektu
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        #endregion
+    }
+}
---
+++ Aptekopol/Aptekopol/DAL/Repo/Clients.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Aptekopol.DAL.Repo
+{
+    using Entities;
+    internal class Clients
+    {
+        #region Queries
+        private const string GET_ALL_CLIENTS = "SELECT * FROM `clients`";
+        private const string INSERT_INTO_CLIENTS = "INSERT INTO `clients`(`Login`, `Firstname`, `Surname`, `Is_company`, `Name`, " +
+            "`NIP`, `City`, `Address`, `Phone`, `Email`, `Password`, `Password_last_change`,) VALUES ";
+        #endregion
+
+        #region CRUD
+        // Pobranie wszystkich klientów z DB i stworzenie z nich listy
+        public static List<Client> GetAllClients()
+        {
+            List<Client> clients = new List<Client>();
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand(GET_ALL_CLIENTS, connection);
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                    clients.Add(new Client(reader));
+
+                connection.Close();
+            }
+            return clients;
+        }
+
+        public static Client GetClientByID(int id)
+        {
+            Client client = null;
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                string UPDATE = $" WHERE ID LIKE {id}";
+
+                MySqlCommand command = new MySqlCommand($"{GET_ALL_CLIENTS} {UPDATE}", connection);
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+                client = new Client(reader);
+
+                connection.Close();
+            }
+            return client;
+        }
+
+        // Dodanie nowego klienta do BD
+        public static bool AddClient(Client client)
+        {
+            bool status = false;
+
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand($"{INSERT_INTO_CLIENTS} {client.ToInsert()}", connection);
+                connection.Open();
+
+                var id = command.ExecuteNonQuery();
+                status = true;
+                client.ID = (sbyte)command.LastInsertedId;
+                connection.Close();
+            }
+
+            return status;
+        }
+
+        // Edycja istniejącego klienta
+        public static bool EditClient(Client client, int clientID)
+        {
+            bool status = false;
+
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                string UPDATE_CLIENT = $"UPDATE `clients` SET " +
+                    $"`Login`='{client.Login}'," +
+                    $"`Firstname`='{client.Firstname}'," +
+                    $"`Surname`='{client.Surname}'," +
+                    $"`Is_company`='{client.IsCompany}'," +
+                    $"`Name`='{client.Name}'," +
+                    $"`NIP`='{client.NIP}'" +
+                    $"`City`='{client.City}'," +
+                    $"`Address`='{client.Address}'," +
+                    $"`Phone`='{client.Phone}'," +
+                    $"`Email`='{client.Email}'," +
+                    $"`Password`='{client.Password}'" +
+                    $"`Password_last_change`='{client.PasswordLastChange}'," +
+                    $" WHERE ID LIKE {clientID}";
+
+                MySqlCommand command = new MySqlCommand(UPDATE_CLIENT, connection);
+                connection.Open();
+
+                var n = command.ExecuteNonQuery();
+                if (n == 1) status = true;
+
+                connection.Close();
+            }
+
+            return status;
+        }
+
+        // Usunięcie klienta z BD
+        public static bool DelClient(Client client)
+        {
+            bool status = false;
+
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                string DEL_CLIENT = $"DELETE FROM `clients` WHERE id LIKE {client.ID}";
+
+                MySqlCommand command = new MySqlCommand(DEL_CLIENT, connection);
+                connection.Open();
+
+                var n = command.ExecuteNonQuery();
+                if (n == 1) status = true;
+
+                connection.Close();
+            }
+
+            return status;
+        }
+        #endregion
+    }
+}
