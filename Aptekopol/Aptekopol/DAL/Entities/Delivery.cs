ad54d84e502918c3b98e015a286214409f4c455e
ejsiik <72543889+ejsiik@users.noreply.github.com>
2022-06-18 19:34:43 +0200

    Deliveries

---
+++ Aptekopol/Aptekopol/DAL/Entities/Delivery.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Aptekopol.DAL.Entities
+{
+    using DAL.Repo;
+    internal class Delivery
+    {
+
+        #region Properties
+        public sbyte? ID { get; set; }
+
+        public int Product_ID { get; set; }
+
+        public int Supplier_ID { get; set; }
+
+        public string Price { get; set; }
+
+        public string Amount { get; set; }
+        #endregion
+
+        #region Constructors
+        // Stowrzenie obiektu na podstawie otrzymanych danych z BD
+        public Delivery(MySqlDataReader reader)
+        {
+            ID = sbyte.Parse(reader["ID"].ToString());
+            Product_ID = int.Parse(reader["Product_ID"].ToString());
+            Supplier_ID = int.Parse(reader["Supplier_ID"].ToString());
+            Price = reader["Price"].ToString();
+            Amount = reader["Amount"].ToString();
+        }
+
+        // Stworzenie obiektu na podstawie podanych danych z ID NULL
+        public Delivery(int product_ID, int supplier_ID, string price, string amount)
+        {
+            ID = null;
+            Product_ID = product_ID;
+            Supplier_ID = supplier_ID;
+            Price = price;
+            Amount = amount;
+        }
+
+        public Delivery(Delivery delivery)
+        {
+            ID = null;
+            Product_ID = delivery.Product_ID;
+            Supplier_ID = delivery.Supplier_ID;
+            Price = delivery.Price;
+            Amount = delivery.Amount; 
+        }
+        #endregion
+
+        #region Methods
+        // Metoda generująca string dla INSERT TO
+        public string ToInsert()
+        {
+            return $"('{Product_ID}', '{Supplier_ID}', '{Price}', '{Amount}')";
+        }
+
+        // Przeciążenie metody Equals
+        public override bool Equals(object obj)
+        {
+            var delivery = obj as Delivery;
+            if (delivery is null) return false;
+            if (Product_ID != delivery.Product_ID) return false;
+            if (Supplier_ID != delivery.Supplier_ID) return false;
+            if (Price != delivery.Price) return false;
+            if (Amount != delivery.Amount) return false;
+   
+            return true;
+        }
+
+        // Funkcja do zwracania hashu obiektu
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        // Funkcja do pobrania sklepu do którego przypisany jest kontrakt 
+        public Product GetProductByID()
+        {
+            // dodać funkcję przy produktach
+            Product product = null;//  = Products.GetProductByID(Product_ID);
+            return product;
+        }
+
+        // Funkcja do pobrania pracownika do którego przypisany jest kontrakt
+        public Supplier GetSupplierByDelivery()
+        {
+           Supplier supplier = Suppliers.GetSupplierByID(Supplier_ID);
+            return supplier;
+        }
+        #endregion
+    }
+}
---
+++ Aptekopol/Aptekopol/DAL/Repo/Deliveries.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Aptekopol.DAL.Repo
+{
+    using Entities;
+    internal class Deliveries
+    {
+        #region Queries
+        private const string GET_ALL_SUPPLIERS = "SELECT * FROM `suppliers`";
+        private const string GET_ALL_DELIVERIES = "SELECT * FROM `delivery` WHERE `Supplier_ID` LIKE ";
+        private const string INSERT_INTO_SUPPLIERS = "INSERT INTO `suppliers`(`Name`, `City`, `Address`, `Phone`, `Email`, `NIP`, `Remarks`) VALUES ";
+        #endregion
+
+        #region CRUD
+        // Pobranie wszystkich dostawców z DB i stworzenie z nich listy
+        public static List<Supplier> GetAllSuppliers()
+        {
+            List<Supplier> suppliers = new List<Supplier>();
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand(GET_ALL_SUPPLIERS, connection);
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                    suppliers.Add(new Supplier(reader));
+
+                connection.Close();
+            }
+            return suppliers;
+        }
+
+        public static Supplier GetSupplierByID(int id)
+        {
+            Supplier supplier = null;
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                string UPDATE_SUPPLIER = $" WHERE ID LIKE {id}";
+
+                MySqlCommand command = new MySqlCommand($"{GET_ALL_SUPPLIERS} {UPDATE_SUPPLIER}", connection);
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+                supplier = new Supplier(reader);
+
+                connection.Close();
+            }
+            return supplier;
+        }
+
+        public static List<Delivery> GetAllDeliveriesBySupplier(Supplier supplier)
+        {
+            List<Delivery> deliveries = new List<Delivery>();
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand($"{GET_ALL_DELIVERIES} {supplier.ID}", connection);
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                    deliveries.Add(new Delivery(reader));
+
+                connection.Close();
+            }
+            return deliveries;
+        }
+
+        // Dodanie nowego dostawcy do BD
+        public static bool AddSupplier(Supplier supplier)
+        {
+            bool status = false;
+
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand($"{INSERT_INTO_SUPPLIERS} {supplier.ToInsert()}", connection);
+                connection.Open();
+
+                var id = command.ExecuteNonQuery();
+                status = true;
+                supplier.ID = (sbyte)command.LastInsertedId;
+                connection.Close();
+            }
+
+            return status;
+        }
+
+        // Edycja istniejącego
+        public static bool EditSupplier(Supplier supplier, int supplierID)
+        {
+            bool status = false;
+
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                string UPDATE_SUPPLIER = $"UPDATE `suppliers` SET " +
+                    $"`Name`='{supplier.Name}'," +
+                    $"`City`='{supplier.City}'," +
+                    $"`Address`='{supplier.Address}'," +
+                    $"`Phone`='{supplier.Phone}'," +
+                    $"`Email`='{supplier.Email}'" +
+                    $"`NIP`='{supplier.NIP}'" +
+                    $"`Remarks`='{supplier.Remarks}'" +
+                    $" WHERE ID LIKE {supplierID}";
+
+                MySqlCommand command = new MySqlCommand(UPDATE_SUPPLIER, connection);
+                connection.Open();
+
+                var n = command.ExecuteNonQuery();
+                if (n == 1) status = true;
+
+                connection.Close();
+            }
+
+            return status;
+        }
+
+        // Usunięcie z BD
+        public static bool DelSupplier(Supplier supplier)
+        {
+            bool status = false;
+
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                string DEL_SUPPLIER = $"DELETE FROM `suppliers` WHERE id LIKE {supplier.ID}";
+
+                MySqlCommand command = new MySqlCommand(DEL_SUPPLIER, connection);
+                connection.Open();
+
+                var n = command.ExecuteNonQuery();
+                if (n == 1) status = true;
+
+                connection.Close();
+            }
+
+            return status;
+        }
+        #endregion
+    }
+}
