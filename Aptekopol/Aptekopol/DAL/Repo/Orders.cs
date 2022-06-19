7c9077b5a05b171e575e58ac32d519bb1b29ba6e
ejsiik <72543889+ejsiik@users.noreply.github.com>
2022-06-19 15:45:54 +0200

    Orders.cs
    Dużo do naprawy

--- Aptekopol/Aptekopol/DAL/Repo/Orders.cs
+++ Aptekopol/Aptekopol/DAL/Repo/Orders.cs
@@ -33,35 +33,27 @@ namespace Aptekopol.DAL.Repo
             {
                 MySqlCommand command = new MySqlCommand(GET_ALL_SHOPS, connection);
                 connection.Open();
-
                 var reader = command.ExecuteReader();
-
                 while (reader.Read())
                     shops.Add(new Shop(reader));
-
                 connection.Close();
             }
             return shops;
         }
-
         public static Shop GetShopByID(int id)
         {
             Shop shop = null;
             using (var connection = DBConnection.Instance.Connection)
             {
                 string UPDATE_SHOP = $" WHERE ID LIKE {id}";
-
                 MySqlCommand command = new MySqlCommand($"{GET_ALL_SHOPS} {UPDATE_SHOP}", connection);
                 connection.Open();
-
                 var reader = command.ExecuteReader();
                 shop = new Shop(reader);
-
                 connection.Close();
             }
             return shop;
         }
-
         public static List<Contract> GetAllContractsByShop(Shop shop)
         {
             List<Contract> contracts = new List<Contract>();
@@ -69,41 +61,32 @@ namespace Aptekopol.DAL.Repo
             {
                 MySqlCommand command = new MySqlCommand($"{GET_ALL_CONTRACTS} {shop.ID}", connection);
                 connection.Open();
-
                 var reader = command.ExecuteReader();
-
                 while (reader.Read())
                     contracts.Add(new Contract(reader));
-
                 connection.Close();
             }
             return contracts;
         }
-
         // Dodanie nowego sklepu do BD
         public static bool AddShop(Shop shop)
         {
             bool status = false;
-
             using (var connection = DBConnection.Instance.Connection)
             {
                 MySqlCommand command = new MySqlCommand($"{INSERT_INTO_SHOPS} {shop.ToInsert()}", connection);
                 connection.Open();
-
                 var id = command.ExecuteNonQuery();
                 status = true;
                 shop.ID = (sbyte)command.LastInsertedId;
                 connection.Close();
             }
-
             return status;
         }
-
         // Edycja istniejącego
         public static bool EditShop(Shop shop, int shopID)
         {
             bool status = false;
-
             using (var connection = DBConnection.Instance.Connection)
             {
                 string UPDATE_SHOP = $"UPDATE `shops` SET " +
@@ -112,37 +95,27 @@ namespace Aptekopol.DAL.Repo
                     $"`Phone`='{shop.Phone}'," +
                     $"`Email`='{shop.Email}'" +
                     $" WHERE ID LIKE {shopID}";
-
                 MySqlCommand command = new MySqlCommand(UPDATE_SHOP, connection);
                 connection.Open();
-
                 var n = command.ExecuteNonQuery();
                 if (n == 1) status = true;
-
                 connection.Close();
             }
-
             return status;
         }
-
         // Usunięcie z BD
         public static bool DelShop(Shop shop)
         {
             bool status = false;
-
             using (var connection = DBConnection.Instance.Connection)
             {
                 string DEL_SHOP = $"DELETE FROM `shops` WHERE id LIKE {shop.ID}";
-
                 MySqlCommand command = new MySqlCommand(DEL_SHOP, connection);
                 connection.Open();
-
                 var n = command.ExecuteNonQuery();
                 if (n == 1) status = true;
-
                 connection.Close();
             }
-
             return status;
         }
         #endregion*/
