81c569587d8932c08eb494ce6fc855cd74cdf893
ejsiik <72543889+ejsiik@users.noreply.github.com>
2022-06-19 15:24:37 +0200

    Order.cs dodany
    orders.cs do ogarnięcia - za dużo kluczy obcych

---
+++ Aptekopol/Aptekopol/DAL/Entities/Order.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Aptekopol.DAL.Entities
+{
+    using DAL.Repo;
+    internal class Order
+    {
+        #region Properties
+        public sbyte? Order_ID { get; set; }
+
+        public int Client_ID { get; set; }
+
+        public int Shop_ID { get; set; }
+        
+        public int Worker_ID { get; set; }
+
+        public int Product_ID { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime Order_date { get; set; } 
+        #endregion
+
+        #region Constructors
+        // Stowrzenie obiektu na podstawie otrzymanych danych z BD
+        public Order(MySqlDataReader reader)
+        {
+            Order_ID = sbyte.Parse(reader["Order_ID"].ToString());
+            Client_ID = int.Parse(reader["Client_ID"].ToString());
+            Shop_ID = int.Parse(reader["Shop_ID"].ToString());
+            Worker_ID = int.Parse(reader["Worker_ID"].ToString());
+            Product_ID = int.Parse(reader["Product_ID"].ToString());
+            Quantity = int.Parse(reader["Quantity"].ToString());
+            Order_date = DateTime.Parse(reader["Order_date"].ToString());
+        }
+
+        // Stworzenie obiektu na podstawie podanych danych z ID NULL
+        public Order(int orderID, int clientID, int shopID, int workerID, int productID, int quantity, DateTime orderdate)
+        {
+            Order_ID = null;
+            Client_ID = clientID;
+            Shop_ID = shopID;
+            Worker_ID = workerID;
+            Product_ID = productID;
+            Quantity = quantity;
+            Order_date = orderdate;
+        }
+
+        public Order(Order order)
+        {
+            Order_ID = order.Order_ID;
+            Client_ID = order.Client_ID;
+            Shop_ID = order.Shop_ID;
+            Worker_ID = order.Worker_ID;
+            Product_ID = order.Product_ID;
+            Quantity = order.Quantity;
+            Order_date = order.Order_date;           
+        }
+        #endregion
+
+        #region Methods
+        // Metoda generująca string dla INSERT TO
+        public string ToInsert()
+        {
+            return $"('{Order_ID}', '{Client_ID}', '{Shop_ID}', '{Worker_ID}', '{Product_ID}', '{Quantity}', '{Order_date}')";
+        }
+
+        // Przeciążenie metody Equals
+        public override bool Equals(object obj)
+        {
+            var order = obj as Order;
+            if (order is null) return false;
+            if (Order_ID != order.Order_ID) return false;
+            if (Client_ID != order.Client_ID) return false;
+            if (Shop_ID != order.Shop_ID) return false;
+            if (Worker_ID != order.Worker_ID) return false;
+            if (Product_ID != order.Product_ID) return false;
+            if (Quantity != order.Quantity) return false;
+            if (!Order_date.Equals(order.Order_date)) return false;
+
+            return true;
+        }
+
+        // Funkcja do zwracania hashu obiektu
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        #endregion
+    }
+}
---
+++ Aptekopol/Aptekopol/DAL/Repo/Orders.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Aptekopol.DAL.Repo
+{
+    using Entities;
+    internal class Orders
+    {
+        //logika dla tych foreigh id mnie przerasta 
+        #region Queries
+        private const string GET_ALL_CLIENTS = "SELECT * FROM `clients`";
+        private const string GET_ALL_SHOPS = "SELECT * FROM `shops`";
+        private const string GET_ALL_WORKERS = "SELECT * FROM `workers`";
+        private const string GET_ALL_PRODUCTS = "SELECT * FROM `products`";
+        private const string GET_ALL_ORDERS = "SELECT * FROM `orders` WHERE `shop_id` LIKE ";
+        private const string INSERT_INTO_SHOPS = "INSERT INTO `shops`(`City`, `Address`, `Phone`, `Email`) VALUES ";
+        //private const string INSERT_INTO_SHOPS
+        //private const string INSERT_INTO_SHOPS
+        //private const string INSERT_INTO_SHOPS
+        #endregion
+
+        /*
+        #region CRUD
+        // Pobranie wszystkich sklepów z DB i stworzenie z nich listy
+        public static List<Shop> GetAllShops()
+        {
+            List<Shop> shops = new List<Shop>();
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand(GET_ALL_SHOPS, connection);
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                    shops.Add(new Shop(reader));
+
+                connection.Close();
+            }
+            return shops;
+        }
+
+        public static Shop GetShopByID(int id)
+        {
+            Shop shop = null;
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                string UPDATE_SHOP = $" WHERE ID LIKE {id}";
+
+                MySqlCommand command = new MySqlCommand($"{GET_ALL_SHOPS} {UPDATE_SHOP}", connection);
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+                shop = new Shop(reader);
+
+                connection.Close();
+            }
+            return shop;
+        }
+
+        public static List<Contract> GetAllContractsByShop(Shop shop)
+        {
+            List<Contract> contracts = new List<Contract>();
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand($"{GET_ALL_CONTRACTS} {shop.ID}", connection);
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                    contracts.Add(new Contract(reader));
+
+                connection.Close();
+            }
+            return contracts;
+        }
+
+        // Dodanie nowego sklepu do BD
+        public static bool AddShop(Shop shop)
+        {
+            bool status = false;
+
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand($"{INSERT_INTO_SHOPS} {shop.ToInsert()}", connection);
+                connection.Open();
+
+                var id = command.ExecuteNonQuery();
+                status = true;
+                shop.ID = (sbyte)command.LastInsertedId;
+                connection.Close();
+            }
+
+            return status;
+        }
+
+        // Edycja istniejącego
+        public static bool EditShop(Shop shop, int shopID)
+        {
+            bool status = false;
+
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                string UPDATE_SHOP = $"UPDATE `shops` SET " +
+                    $"`City`='{shop.City}'," +
+                    $"`Address`='{shop.Address}'," +
+                    $"`Phone`='{shop.Phone}'," +
+                    $"`Email`='{shop.Email}'" +
+                    $" WHERE ID LIKE {shopID}";
+
+                MySqlCommand command = new MySqlCommand(UPDATE_SHOP, connection);
+                connection.Open();
+
+                var n = command.ExecuteNonQuery();
+                if (n == 1) status = true;
+
+                connection.Close();
+            }
+
+            return status;
+        }
+
+        // Usunięcie z BD
+        public static bool DelShop(Shop shop)
+        {
+            bool status = false;
+
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                string DEL_SHOP = $"DELETE FROM `shops` WHERE id LIKE {shop.ID}";
+
+                MySqlCommand command = new MySqlCommand(DEL_SHOP, connection);
+                connection.Open();
+
+                var n = command.ExecuteNonQuery();
+                if (n == 1) status = true;
+
+                connection.Close();
+            }
+
+            return status;
+        }
+        #endregion*/
+    }
+}
